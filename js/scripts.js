07e4a5cd7a827e9b9b78e117426972ae7f0553d9
Ivan Lebedzeu <ivan.lebedzeu@gmail.com>
2020-03-23 13:05:35 -0700

    all calculation functions are added

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
 var add = function(number1, number2) {
   return number1 + number2;
 };
+var sub  = function(number1, number2) {
+  return number1 - number2;
+};
 
+var mult  = function(number1, number2) {
+  return number1 * number2;
+};
+
+var dev  = function(number1, number2) {
+  return number1 / number2;
+};
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 
-alert(add(number1, number2));
+//alert(add(number1, number2));
+alert(sub(number1, number2));
+//alert(mult(number1, number2));
+//alert(dev(number1, number2));
+
