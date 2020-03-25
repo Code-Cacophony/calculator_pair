4dcb75e321d0349dbf5cec149b83a09c5e2f2d9e
Ivan Lebedzeu <ivan.lebedzeu@gmail.com>
2020-03-25 10:47:44 -0700

    it will show user interface for calculator with business logic (add 2 numbers)

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business (or back-end) logic:
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -12,8 +14,20 @@ var mult  = function(number1, number2) {
 var dev  = function(number1, number2) {
   return number1 / number2;
 };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+
+
+// Everything below this line is user interface (or front-end) logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+    //alert(add(number1, number2));
+  });
+});
 
 //alert(add(number1, number2));
 alert(sub(number1, number2));
